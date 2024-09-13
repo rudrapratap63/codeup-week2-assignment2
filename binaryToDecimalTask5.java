d543b93de37e8bd42247a3a83cec533ade07f593
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2024-09-13 10:47:21 +0530

    Update binaryToDecimalTask5.java
    added working of point binary format

--- binaryToDecimalTask5.java
+++ binaryToDecimalTask5.java
@@ -8,38 +8,75 @@ Date -> 11/09/2024
 import java.util.Scanner;
 
 public class binaryToDecimalTask5 {
-    static long binaryNumber;
+    static double decimalNumberAfterDecimal;
+    static boolean isNoError;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         try {
-            System.out.println("Enter a Integer in binary format(Non Decimal) :");
-            binaryNumber = input.nextLong();
-            if (binaryNumber<0) {
-                System.out.println("Please enter only positive binary number");
+            System.out.println("Enter a Integer in binary format :");
+            String binaryDecimal = input.nextLine();
+            String[] binaryArray = binaryDecimal.split("\\.");
+            if ((Long.parseLong(binaryArray[0])) < 0) {
+                System.out.println("please enter only positive integer");
                 input.close();
                 return;
             }
-            binaryToDecimal(binaryNumber, 0, 0);
+            if (binaryArray.length > 2) {
+                System.out.println("Please enter correct decimal binary value");
+                input.close();
+                return;
+            }
+            binaryToDecimal(Long.parseLong(binaryArray[0]), 0, 0);
+            if (binaryArray.length == 2) {
+                binaryDecimalToDecimal(Integer.parseInt(binaryArray[1]), 0, -binaryArray[1].toCharArray().length);
+            }
+            if (isNoError) {
+                System.out.println("Decimal number is : " + decimalNumberAfterDecimal);
+            }
         } catch (Exception e) {
             System.out.println("invalid input ");
         }
-
         input.close();
     }
 
-    public static void binaryToDecimal(long binaryNumber, long decimalNumber,int index){
+    // store the value of binary to decimal before point in variable
+    // Input -> Long, Long, Integer
+    // Return type -> void
+    public static void binaryToDecimal(long binaryNumber, long decimalNumber, int index) {
         if (binaryNumber > 0) {
             long binarydigit = (binaryNumber % 10);
-            if (binarydigit != 0 && binarydigit != 1 ) {
+            if (binarydigit != 0 && binarydigit != 1) {
                 System.out.println("Error: Please enter input in binary format");
                 return;
             }
-            decimalNumber = decimalNumber + binarydigit*(int)(Math.pow(2, index));
-            binaryNumber = binaryNumber/10;
+            decimalNumber = decimalNumber + binarydigit * (int) (Math.pow(2, index));
+            binaryNumber = binaryNumber / 10;
             binaryToDecimal(binaryNumber, decimalNumber, ++index);
-        }else{
-            System.out.println("Decimal Representation of binary number is : \n"+decimalNumber);
+        } else {
+            decimalNumberAfterDecimal += decimalNumber;
+            isNoError = true;
+            return;
+        }
+    }
+
+    // store the value of binary to decimal after point in variable with addition
+    // Input -> Long, double, Integer
+    // Return type -> void
+    public static void binaryDecimalToDecimal(long binaryNumber, double decimalNumber, int index) {
+        if (index < 0) {
+            double binarydigit = (binaryNumber % 10);
+            if (binarydigit != 0 && binarydigit != 1) {
+                System.out.println("Error: Please enter input in binary format");
+                isNoError = isNoError & false;
+                return;
+            }
+            decimalNumber = decimalNumber + binarydigit * (Math.pow(2, index));
+            binaryNumber = binaryNumber / 10;
+            binaryDecimalToDecimal(binaryNumber, decimalNumber, ++index);
+        } else {
             System.out.println();
+            decimalNumberAfterDecimal += decimalNumber;
             return;
         }
     }
